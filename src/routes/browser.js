716813e003bfe34f62ec9630422a73ed413b8fda
trainningjava <48299029+trainningjava@users.noreply.github.com>
2020-08-11 01:43:09 -0300

    Upd

--- src/routes/browser.js
+++ src/routes/browser.js
@@ -3,50 +3,36 @@
 const express = require("express");
 let router = express.Router();
 
-router
-  .route("/login")
-  .get((req, res) => {
-    return res.render("create-account.html");
-  })
+router.route("/login").get((req, res) => {
+  return res.render("create-account.html");
+});
 
-  .route("/create-tax")
-  .get((req, res) => {
-    return res.render("consumo.html");
-  })
+router.route("/smart").get((req, res) => {
+  return res.render("smart.html");
+});
 
-  .route("/smart")
-  .get((req, res) => {
-    return res.render("smart.html");
-  })
+router.route("/dispositivo").get((req, res) => {
+  return res.render("dispositivo.html");
+});
 
-  .route("/dispositivo")
-  .get((req, res) => {
-    return res.render("dispositivo.html");
-  })
+router.route("/calculadora").get((req, res) => {
+  return res.render("calculadora.html");
+});
 
-  .route("/calculadora")
-  .get((req, res) => {
-    return res.render("calculadora.html");
-  })
+router.route("/configuracao").get((req, res) => {
+  return res.render("configuracao.html");
+});
 
-  .route("/configuracao")
-  .get((req, res) => {
-    return res.render("configuracao.html");
-  })
+router.route("/cadastrodispositivo").get((req, res) => {
+  return res.render("cadastro-dispositivo.html");
+});
 
-  .route("/cadastrodispositivo")
-  .get((req, res) => {
-    return res.render("cadastro-dispositivo.html");
-  })
+router.route("/consumo").get((req, res) => {
+  return res.render("consumo.html");
+});
 
-  .route("/consumo")
-  .get((req, res) => {
-    return res.render("consumo.html");
-  })
-
-  .route("/cad-panel")
-  .get((req, res) => {
-    return res.render("cad-panel.html");
-  });
+router.route("/cad-panel").get((req, res) => {
+  return res.render("cad-panel.html");
+});
 
 module.exports = router;
